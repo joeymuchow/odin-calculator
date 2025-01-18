eea3a0b4342ebf0cc0d8e58571aea56bd5747d19
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-17 23:26:30 -0800

    Create event listeners for buttons and display
    Event listeners have been added to the buttons and work at a base level.
    They are also all hooked up to the display so you can see the numbers
    as you type them in and see the result of your equation.

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
-let operator;
-let number1;
-let number2;
+let operator ="";
+let number1 = "";
+let number2 = "";
+const display = document.querySelector(".display");
 
 function add(num1, num2) {
     return num1 + num2;
@@ -19,20 +20,57 @@ function divide(num1, num2) {
 }
 
 function operate(operator, num1, num2) {
+    let result;
     switch(operator) {
         case "+":
-            add(num1, num2);
+            result = add(num1, num2);
             break;
         case "-":
-            subtract(num1, num2);
+            result = subtract(num1, num2);
             break;
         case "x":
-            multiply(num1, num2);
+            result = multiply(num1, num2);
             break;
         case "/":
-            divide(num1, num2);
+            result = divide(num1, num2);
             break;
         default:
             console.log("Unknown operator");
     }
+    return result;
 }
+
+const digitButtons = document.querySelectorAll(".numbers button");
+digitButtons.forEach(element => {
+    element.addEventListener("click", (e) => {
+        if (operator) {
+            number2 += e.target.textContent;
+        } else {
+            number1 += e.target.textContent;
+        }
+        display.textContent = `${number1} ${operator} ${number2}`;
+    })
+});
+
+const operators = document.querySelectorAll(".operators");
+operators.forEach(element => {
+    element.addEventListener("click", (e) => {
+        // TODO update this so that if there are two numbers and
+        // the user presses an operator again instead of equals
+        // the equation happens for the first operator and the 
+        // result is displayed with the second operator now added
+        operator = e.target.textContent;
+        display.textContent = `${number1} ${operator} ${number2}`;
+    });
+});
+
+document.querySelector(".equals").addEventListener("click", () => {
+    display.textContent = operate(operator, Number(number1), Number(number2));
+});
+
+document.querySelector(".clear").addEventListener("click", () => {
+    display.textContent = "0";
+    operator = "";
+    number1 = "";
+    number2 = "";
+});
