1d9c0ca55655f6661f5743386e78903759ba552f
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-18 23:11:08 -0800

    Fix divide edge cases and operator multi-press
    I fixed the divide function so that it will round decimals to 2 places
    and set a check for divide by zero to message the user. Also added code
    to handle when a user presses another operator before equals and show
    the new result with the new operator.

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
 let operator ="";
-let number1 = "";
+let number1 = "0";
 let number2 = "";
 const display = document.querySelector(".display");
+const message = document.querySelector(".message");
 
 function add(num1, num2) {
     return num1 + num2;
@@ -16,10 +17,20 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    if (num2 === 0) {
+        message.textContent = "DON'T DIVIDE BY ZERO!!!"
+        return 0;
+    }
+    const solution = num1 / num2;
+    if (solution % 1 === 0) {
+        return solution;
+    }
+    return solution.toFixed(2);
 }
 
 function operate(operator, num1, num2) {
+    num1 = Number(num1);
+    num2 = Number(num2);
     let result;
     switch(operator) {
         case "+":
@@ -43,10 +54,11 @@ function operate(operator, num1, num2) {
 const digitButtons = document.querySelectorAll(".numbers button");
 digitButtons.forEach(element => {
     element.addEventListener("click", (e) => {
+        message.textContent = "";
         if (operator) {
             number2 += e.target.textContent;
         } else {
-            number1 += e.target.textContent;
+            number1 === "0" ? number1 = e.target.textContent : number1 += e.target.textContent;
         }
         display.textContent = `${number1} ${operator} ${number2}`;
     })
@@ -55,22 +67,29 @@ digitButtons.forEach(element => {
 const operators = document.querySelectorAll(".operators");
 operators.forEach(element => {
     element.addEventListener("click", (e) => {
-        // TODO update this so that if there are two numbers and
-        // the user presses an operator again instead of equals
-        // the equation happens for the first operator and the 
-        // result is displayed with the second operator now added
-        operator = e.target.textContent;
+        message.textContent = "";
+        if (operator && number1 && number2) {
+            number1 = operate(operator, number1, number2);
+            number2 = "";
+            operator = e.target.textContent;
+        } else if (number1) {
+            operator = e.target.textContent;
+        }
         display.textContent = `${number1} ${operator} ${number2}`;
     });
 });
 
 document.querySelector(".equals").addEventListener("click", () => {
-    display.textContent = operate(operator, Number(number1), Number(number2));
+    message.textContent = "";
+    if (operator && number1 && number2) {
+        display.textContent = operate(operator, number1, number2);
+    }
 });
 
 document.querySelector(".clear").addEventListener("click", () => {
     display.textContent = "0";
+    message.textContent = "";
     operator = "";
-    number1 = "";
+    number1 = "0";
     number2 = "";
 });
